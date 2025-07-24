200a904f4eb7bd30115bc6cbb1f6a5c584c8daf9
Phuwadol Viroonluecha <shusei@gmail.com>
2025-07-24 12:20:00 -0700

    2025 Profile updated
    - Revised Profile on homepage
    - Added a new article about PID T1D
    - Added dashboards and projects

--- js/footer.js
+++ js/footer.js
@@ -28,7 +28,7 @@ function appendContentToFooter() {
 
     const copyrightText = document.createElement("p");
     copyrightText.className = "m-0";
-    copyrightText.innerHTML = "&copy; 2024 <a class='text-white font-weight-bold' href='#'>Phuwadol Viroonluecha's Personal Website - ภูวดล วิรุฬห์ลือชา</a>";
+    copyrightText.innerHTML = "&copy; 2025 <a class='text-white font-weight-bold' href='#'>Phuwadol Viroonluecha's Personal Website - ภูวดล วิรุฬห์ลือชา</a>";
 
     container.appendChild(socialMediaContainer);
     container.appendChild(copyrightText);
